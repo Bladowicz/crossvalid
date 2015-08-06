1e534f2c4a881835dc38b7467d6cb7ce88707f06
DataScience-Team <web.analytics@adpilot.pl>
2015-08-06 20:25:41 +0200

    Initial

---
+++ main.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+import os, sys
+import src
+from src import *
+import logging
+import filter_features.main as ff
+import string
+import random
+import itertools
+
+
+
+def create_temp_folder(base):
+    token = ''.join([random.choice(string.hexdigits) for x in range(4)])
+    temp_folder = os.path.join(base, 'temp_'+token)
+    if not os.path.exists(temp_folder):
+            os.makedirs(temp_folder)
+    logging.info('Temp folder created \n {}'.format(temp_folder))
+    return temp_folder
+
+def preparefile(infile, temp_folder, mincount):
+    logging.info('Reforming file')
+    reformed = os.path.join(temp_folder, 'reformed')
+    os.system('sort {} | uniq | shuf > {}'.format(infile, reformed))
+    logging.info('Removing features with less than {}'.format(mincount))
+    filtered_reformed = os.path.join(temp_folder, 'filtered')
+    ff.filter_features(reformed, filtered_reformed, mincount)
+    return filtered_reformed
+
+def spliting_files(parts, infile, temp_folder):
+    file_list = []
+    for part in range(parts):
+        prefix = '{}_{}'.format(part + 1, parts)
+        logging.info('Making files for {}'.format(prefix))
+        part_dir = os.path.join(temp_folder, prefix)
+        os.makedirs(part_dir)
+        part_files = (prefix, 
+                os.path.join(part_dir, 'trening.vw'),
+                os.path.join(part_dir, 'test.vw'),
+                os.path.join(part_dir, 'model.vw'),
+                os.path.join(part_dir, 'pred.txt'),
+                )
+        ftren, ftest = open(part_files[1], 'w'), open(part_files[2], 'w')
+
+        for nr, line in enumerate(open(infile)):
+            if nr % parts == part:
+                ftest.write(line)
+            else:
+                ftren.write(line)
+        file_list.append(part_files)
+    return file_list
+
+def run_vopal(part, temp_folder):
+    q = query.format(in_file=part[1], out_file=part[3])
+    os.system(q)
+    p = pred.format(out_file=part[3], test_file=part[2], pred_file=part[4])
+    os.system(p)
+    
+
+def crossvalidation(parts, infile, temp_base, outfile, mincount):
+    temp_folder = create_temp_folder(temp_base)
+    logging.info('Starting crossvalidation for file : {}'.format(infile) )
+    preparedfile = preparefile(infile, temp_folder, mincount)
+    splited_files = spliting_files(parts, preparedfile, temp_folder)
+    for part in splited_files:
+        run_vopal(part , temp_folder)
+    with open(outfile, 'w') as fw:
+        for part in splited_files:
+            logging.info('Merging part {}'.format(part[0]))
+            for line_a, line_b in itertools.izip(open(part[2]), open(part[4])):
+                fw.write('\t'.join([line_a.split()[0], line_b.split()[0]] )+'\n')
+    logging.warning('---| rm -rf {}'.format(temp_folder))
+
+
+def main(parts, infile, temp_base, outfile, mincount):
+    crossvalidation(parts, infile, temp_base, outfile,  mincount)
+
+
+if __name__=="__main__":
+    config = input_parser._get()
+    mincount = config.mincount
+    parts = config.parts
+    infile = config.in_file
+    temp_base = config.temp_location
+    outfile = config.out_file
+    query = "vw {in_file} -k -f {out_file} --cache_file {out_file}_cache -b 22 --passes 10 --meanfield --multitask --nn 13 --keep b --keep h --ignore u --keep r --interactions bb --ftrl --loss_function logistic"
+    pred = "vw -d {test_file} -t -i {out_file}  -p {pred_file}"
+    
+    main(parts, infile, temp_base, outfile,  mincount)
+
+
+
---
+++ src/input_parser.py
@@ -0,0 +1,65 @@
+import argparse
+import logging
+import os
+import sys
+
+def validate_args(conf):
+    conf.in_file = conf.in_file[0]
+    conf.out_file = conf.out_file[0]
+#    conf.product = conf.product[0]
+#    if not os.path.exists(conf.in_file):
+#        logging.error('Input file does not exist')
+#        sys.exit()
+#    else:
+#        if not os.access(conf.in_file, 4):
+#            logging.error('Dont have permissions to read input file.')
+#            sys.exit()
+#
+#    if os.path.exists(conf.out_file):
+#        if conf.force_overwrite:
+#            logging.warning('Output file exists - overwriting.')
+#            if not os.access(conf.out_file, 2):
+#                logging.error('Dont have permissions to write in output file.')
+#                sys.exit()
+#        else:
+#            logging.error('Output file already exists. Please change name, of add -F')
+#            sys.exit()
+#
+
+
+def _get():
+    parser = argparse.ArgumentParser()
+ #   sp = parser.add_subparsers()
+    method = parser.add_mutually_exclusive_group(required=True)    
+
+    parser.add_argument(action='store', dest='in_file',
+                                help='Input file in Vopal Wabbit input format',
+                                nargs=1,
+                                )
+
+    parser.add_argument(action='store', dest='out_file',
+                                help='Output file.',
+                                nargs=1,
+                                )
+
+
+    method.add_argument('-c', action='store', dest='mincount',
+                                help='Min count of occurences',
+                                default=250)
+
+
+    method.add_argument('-p', action='store', dest='parts',
+                                help='Parts to fold',
+                                default=4)
+
+
+    method.add_argument('-t', action='store', dest='temp_location',
+                                help='Location for calculations',
+                                default="/home/model/.crossvalidation/")
+
+
+    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+    out =  parser.parse_args()
+    validate_args(out)
+    return out
+#results = _get()
